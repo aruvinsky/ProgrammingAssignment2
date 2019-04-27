04fb98fc858cb934b1b6ba65de283a086aa36a2f
Alicia Ruvinsky <49073307+aruvinsky@users.noreply.github.com>
2019-04-27 12:18:45 -0500

    Add files via upload
    Instantiated both methods for class assignment:
    makeCacheMatrix
    cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix: 
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
 
+    # x$set:
+    # given a matrix y, this function sets the internal matrix variable of this special matrix to y
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    # x$get:
+    # this function returns the current matrix value, x
+    get <- function() x
+    # x$setInv:
+    # given the inverted matrix of x, this function sets the variable inv to that inverted matrix
+    setInv <- function(inverse) inv <<- inverse
+    # x$getInv:
+    # this function returns the current inverted matrix value of x, namely inv
+    getInv <- function() inv
+
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat <- x$get()
+
+  # Return a matrix that is the inverse of 'x'
+  # first, make sure that the inverse has been calculated and is not NA
+  invMat <- x$getInv()
+  if (!is.null(invMat)) {
+    message("getting cached matrix inverse...")
+    return(invMat)
+  }
+  # if this point is reached, it can only be because the inverse has not yet been calculated
+  # calculate and set the inverse of x
+  invMat <- solve(mat, ...)
+  x$setInv(invMat)
+  return(invMat)
 }
